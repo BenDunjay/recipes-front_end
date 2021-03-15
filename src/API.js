ca1c3c748422624cb778823d4583955887b66c4b
BenDunjay <bendunjay@outlook.com>
2021-03-15 17:54:03 +0000

    test run of routes and connection to users recipes url success

--- src/API.js
+++ src/API.js
@@ -1,3 +1,25 @@
-const baseUrl = "http://localhost:3000/api/v1";
+const baseUrl = "http://localhost:3000/api/v1/";
+const baseUserRecipesUrl = (userId) => baseUrl + "users/" + userId;
+// auth controller
+const userLogin = baseUrl + "login";
 
-const userLogin = baseUrl + "/login";
+// recipes controller
+const allRecipes = baseUrl + "recipes";
+
+// user_recipes controller
+const allUserRecipes = (userId) => baseUserRecipesUrl(userId) + "/my_recipes";
+const updateUserRecipe = (userId) =>
+  baseUserRecipesUrl(userId) + "/users_recipes/";
+// user controller
+
+const get = (url) => {
+  return fetch(url, {
+    headers: { Authorization: localStorage.token },
+  }).then((resp) => resp.json());
+};
+
+const getUsersRecipes = (user) => {
+  return get(allUserRecipes(6));
+};
+
+export default { getUsersRecipes };
